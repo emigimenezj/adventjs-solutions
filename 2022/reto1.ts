26412d7269677e5f799188a49e21c35a6f7685ea
Emilio Giménez <emigimenezj@zohomail.com>
2023-01-17 22:36:25 -0300

    minor fix

--- 2022/reto1.ts
+++ 2022/reto1.ts
@@ -1,5 +1,5 @@
 export function wrapping(gifts: string[]): string[] {
-    return gifts.map(g =>
-      '*'.repeat(g.length+2) + `\n*${g}*\n` + '*'.repeat(g.length+2)
-    );
-  }
+  return gifts.map(g =>
+    '*'.repeat(g.length+2) + `\n*${g}*\n` + '*'.repeat(g.length+2)
+  );
+}
