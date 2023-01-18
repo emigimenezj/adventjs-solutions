618ed42c09bf2032ece471f4181dfe629555f4f8
Emilio Giménez <emigimenezj@zohomail.com>
2023-01-17 22:24:18 -0300

    Agregado reto1 en JS y TS

---
+++ 2022/reto1.js
@@ -0,0 +1,5 @@
+function wrapping(gifts) {
+  return gifts.map(g =>
+    '*'.repeat(g.length+2) + `\n*${g}*\n` + '*'.repeat(g.length+2)
+  );
+}
---
+++ 2022/reto1.ts
@@ -0,0 +1,5 @@
+export function wrapping(gifts: string[]): string[] {
+    return gifts.map(g =>
+      '*'.repeat(g.length+2) + `\n*${g}*\n` + '*'.repeat(g.length+2)
+    );
+  }
